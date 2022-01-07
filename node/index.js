0f7a981e9d6111991c45babf521d10080c7eade8
henriquegon <henrique.goncalves@supersim.com.br>
2022-01-07 16:57:57 -0300

    - Fixing display text

--- node/index.js
+++ node/index.js
@@ -17,17 +17,21 @@ const insert_query = `insert into people(name) values('Henrique')`
 connection.query(insert_query)
 connection.end()
 
-// Displays "{name} Rocks!"
+// Displays
 app.get('/', (req, res) => {
-    const select_query = `select name from people limit 1`
+    const select_query = `select name from people`
     const connection = mysql.createConnection(config)    
     connection.query(select_query, function (err, result) {
         if (err) throw err
-        console.log(result)
-        res.send(result[0].name + " Rocks!")
+        // console.log(result)
+        let text = "<h1>Full Cycle Rocks!</h1>"
+        for (let i = 0; i < result.length; i++){
+            text += "<br>" + result[i].name
+        }
+        // console.log(text)
+        res.send(text)
     })
     connection.end()
-    
 })
 
 app.listen(port, () => {
